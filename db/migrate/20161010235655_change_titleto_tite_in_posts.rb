fce58fe7c5dc8a15fa2d9e20fe0c4ecfb4fd2899
davidmgudeman <davidmgudeman@gmail.com>
2016-10-11 07:05:58 -0700

    Added 2 commits and three comments

---
+++ db/migrate/20161010235655_change_titleto_tite_in_posts.rb
@@ -0,0 +1,4 @@
+class ChangeTitletoTiteInPosts < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
---
+++ db/migrate/20161010235818_change_title_to_tite_in_posts.rb
@@ -0,0 +1,5 @@
+class ChangeTitleToTiteInPosts < ActiveRecord::Migration[5.0]
+  def change
+  rename_column :Posts, :title, :tite
+  end
+end
